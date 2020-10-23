0262262a8c71cf2fb3e538e86463a37895f3dcff
unknown <luiscf1226@gmail.com>
2020-10-23 15:18:16 -0600

    segundo

--- Laboratorio2_LuisFlores/src/laboratorio2_luisflores/Laboratorio2_LuisFlores.java
+++ Laboratorio2_LuisFlores/src/laboratorio2_luisflores/Laboratorio2_LuisFlores.java
@@ -8,11 +8,12 @@ import java.util.Scanner;
 public class Laboratorio2_LuisFlores {
 
     static ArrayList <Detectives> lista=new ArrayList();
-    static ArrayList <casos> lista1=new ArrayList();
-    static ArrayList <casos> lista2=new ArrayList();
-    static ArrayList <casos> lista3=new ArrayList();
-    static ArrayList <casos> lista4=new ArrayList();
-    static ArrayList <casos> lista5=new ArrayList();
+    static ArrayList <casos> listatodo=new ArrayList();
+    static ArrayList <casos> listahom=new ArrayList();
+    static ArrayList <casos> listarobo=new ArrayList();
+    static ArrayList <casos> listasec=new ArrayList();
+    static ArrayList <casos> listares=new ArrayList();
+    static ArrayList <casos> listapen=new ArrayList();
     public static void main(String[] args) {
         Scanner leer=new Scanner(System.in);
         int opcion=0;
@@ -174,10 +175,193 @@ public class Laboratorio2_LuisFlores {
                     }
                 break;
                 case 4:
-                        System.out.println("LISTA DETECTIVES!");
+                    System.out.println("LISTA DETECTIVES!");
                     for (int i = 0; i < lista.size(); i++) {
                         System.out.println(i+". ->   "+lista.get(i));
                     }
+                break;
+                case 5:
+                    if(lista.isEmpty()){
+                        System.out.println("No hay Detective");
+                    }
+                    System.out.println("Registrar caso: ");
+                    String lugar;
+                    int opc;
+                    String descripcion;
+                    String tipo;
+                    String detective;
+                    String estado;
+                    System.out.println("lugar de el caso: ");
+                    lugar=leer.next();
+                    System.out.println("descripcion : ");
+                    descripcion=leer.nextLine();
+                    descripcion=leer.nextLine();
+                    System.out.println("Para el estado escriba: 'resuelto' o 'pendiente' ");
+                    System.out.println("todo lo demas no sera agregado a la lista de resueltos o pendientes");
+                    System.out.println("Estado: ");
+                    estado=leer.next();
+                    System.out.println("LISTA DETECTIVES!");
+                    for (int i = 0; i < lista.size(); i++) {
+                        System.out.println(i+". ->   "+lista.get(i));
+                    }
+                    System.out.println("Escoja Detective: ");
+                    detective=leer.next();
+                    System.out.println("ingrese el tipo de caso:  ");
+                    System.out.println("1. Homicidio");
+                    System.out.println("2. Robo");
+                    System.out.println("3. Secuestro");
+                    opc=leer.nextInt();
+                    if(opc==1){
+                        tipo=" Homicidio";
+                        listatodo.add(new casos(lugar,  descripcion,  tipo, detective,  estado));
+                        listahom.add(new casos(lugar,  descripcion,  tipo, detective,  estado));
+                        if(estado.equals("resuelto")){
+                        listares.add(new casos(lugar,  descripcion,  tipo, detective,  estado));
+                        if(estado.equals("pendiente")){
+                        listapen.add(new casos(lugar,  descripcion,  tipo, detective,  estado));
+                    }
+                    
+                    }
+                        
+                    }
+                    if(opc==2){
+                        tipo=" Robo";
+                        listatodo.add(new casos(lugar,  descripcion,  tipo, detective,  estado));
+                        listarobo.add(new casos(lugar,  descripcion,  tipo, detective,  estado));
+                        if(estado.equals("resuelto")){
+                        listares.add(new casos(lugar,  descripcion,  tipo, detective,  estado));
+                        if(estado.equals("pendiente")){
+                        listapen.add(new casos(lugar,  descripcion,  tipo, detective,  estado));
+                    }
+                    
+                    }
+                    }
+                    if (opc==3){
+                        tipo="Secuestro";
+                        listatodo.add(new casos(lugar,  descripcion,  tipo, detective,  estado));
+                        listasec.add(new casos(lugar,  descripcion,  tipo, detective,  estado));
+                        if(estado.equals("resuelto")){
+                        listares.add(new casos(lugar,  descripcion,  tipo, detective,  estado));
+                        if(estado.equals("pendiente")){
+                        listapen.add(new casos(lugar,  descripcion,  tipo, detective,  estado));
+                    }
+                    
+                    }
+                    }
+                    
+                    
+                    
+                break;
+                case 6:
+                    System.out.println("Modificar caso: ");
+                    int u=0;
+                    while (u!=6){
+                        System.out.println("1. Modificar lugar");
+                        System.out.println("2. Modificar descripcion");
+                        System.out.println("3. Modificar tipo");
+                        System.out.println("4. Modificar detective");
+                        System.out.println("5. Modificar estado");
+                        System.out.println("6. Salir");
+                        u=leer.nextInt();
+                        switch(u){
+                            case 1:
+                                int k;
+                                String b;
+                                System.out.println("Posicion caso a modificar: ");
+                                k=leer.nextInt();
+                                if( k>=0&&k<listatodo.size()){
+                                    if(listatodo.get(k)instanceof casos){
+                                        System.out.println("Ingrese Nuevo lugar: ");
+                                        b=leer.next();
+                                        ((casos)listatodo.get(k)).setLugar(b);
+                                        ((casos)listarobo.get(k)).setLugar(b);
+                                        ((casos)listares.get(k)).setLugar(b);
+                                        ((casos)listasec.get(k)).setLugar(b);
+                                        ((casos)listahom.get(k)).setLugar(b);
+                                        ((casos)listapen.get(k)).setLugar(b);
+                                    }else{
+                                        System.out.println("posicion no es...");
+                                    }
+                                } else{
+                                    System.out.println("posicion fuera rango: ");
+                                    
+                                }
+                            break;
+                            case 2:
+                                int k2;
+                                String b2;
+                                System.out.println("Posicion caso a modificar: ");
+                                k2=leer.nextInt();
+                                if( k2>=0&&k2<listatodo.size()){
+                                    if(listatodo.get(k2)instanceof casos){
+                                        System.out.println("Ingrese Nuevo Descripcion: ");
+                                        b2=leer.next();
+                                        ((casos)listatodo.get(k2)).setDescripcion(b2);
+                                        ((casos)listarobo.get(k2)).setDescripcion(b2);
+                                        ((casos)listares.get(k2)).setDescripcion(b2);
+                                        ((casos)listasec.get(k2)).setDescripcion(b2);
+                                        ((casos)listahom.get(k2)).setDescripcion(b2);
+                                        ((casos)listapen.get(k2)).setDescripcion(b2);
+                                    }else{
+                                        System.out.println("posicion no es...");
+                                    }
+                                } else{
+                                    System.out.println("posicion fuera rango: ");
+                                    
+                                }
+                                
+                            break;
+                            case 3:
+                                int k3;
+                                String b3;
+                                System.out.println("Posicion caso a modificar: ");
+                                k3=leer.nextInt();
+                                if( k3>=0&&k3<listatodo.size()){
+                                    if(listatodo.get(k3)instanceof casos){
+                                        System.out.println("Ingrese Nuevo tipo: ");
+                                        System.out.println("1. Homicidio");
+                                        System.out.println("2. Robo");
+                                        System.out.println("3. Secuestro");
+                                        int opc1=leer.nextInt();
+                                        if(opc1==1){
+                                         b3="Homicidio";
+                                        ((casos)listatodo.get(k3)).setDescripcion(b3);
+                                        ((casos)listarobo.get(k3)).setDescripcion(b3);
+                                        ((casos)listares.get(k3)).setDescripcion(b3);
+                                        ((casos)listasec.get(k3)).setDescripcion(b3);
+                                        ((casos)listahom.get(k3)).setDescripcion(b3);
+                                        ((casos)listapen.get(k3)).setDescripcion(b3);
+                                        }
+                                        if(opc1==2){
+                                         b3="Robo";
+                                        ((casos)listatodo.get(k3)).setDescripcion(b3);
+                                        ((casos)listarobo.get(k3)).setDescripcion(b3);
+                                        ((casos)listares.get(k3)).setDescripcion(b3);
+                                        ((casos)listasec.get(k3)).setDescripcion(b3);
+                                        ((casos)listahom.get(k3)).setDescripcion(b3);
+                                        ((casos)listapen.get(k3)).setDescripcion(b3);
+                                        }
+                                        if(opc1==3){
+                                         b3="Secuestro";
+                                        ((casos)listatodo.get(k3)).setDescripcion(b3);
+                                        ((casos)listarobo.get(k3)).setDescripcion(b3);
+                                        ((casos)listares.get(k3)).setDescripcion(b3);
+                                        ((casos)listasec.get(k3)).setDescripcion(b3);
+                                        ((casos)listahom.get(k3)).setDescripcion(b3);
+                                        ((casos)listapen.get(k3)).setDescripcion(b3);
+                                        }
+          
+                                    }else{
+                                        System.out.println("posicion no es...");
+                                    }
+                                } else{
+                                    System.out.println("posicion fuera rango: ");
+                                    
+                                }
+                                
+                            break;
+                        }
+                    }//fin switch
                 break;
             }//fin switch
         }//fin while
